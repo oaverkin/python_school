c3139d41c64246801055427834849abecc087e3e
Oleg Averkin <averkinoa@gmail.com>
2024-04-05 01:01:17 +0300

    The model of free falling object

--- urok_16.py
+++ urok_16.py
@@ -1,27 +1,11 @@
-import turtle
+speed = int(2)
+height = int(20)
 
-v0 = int(input("Введите начальную скорость: "))
-h = int(input("Введите начальную высоту: "))
-turtle.forward(100)
-turtle.backward(100)
-turtle.left(90)
-turtle.forward(h)
-
-t = 0.1
+time = 1
 x = 0
-y = h
+y = height
 while y > 0:
-    print(f"t={round(t, 1)}, x={round(x, 1)}, y={round(y, 1)}")
-    t += 0.1
-    x = v0 * t
-    y = h - (9.8 * t**2) / 2
-    turtle.goto(x, y)
-
-turtle.up()
-turtle.goto(0, h)
-turtle.write(h)
-turtle.goto(x, 0)
-turtle.write(x)
-
-
-
+    print(f"time={round(time, 1)}, x={round(x, 1)}, y={round(y, 1)}")
+    time += 0.1
+    x = speed * time
+    y = height - (9.8 * time**2) / 2
