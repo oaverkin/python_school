3f9f96748224d9e7b470ede7c9994d861f3d9abe
Oleg Averkin <averkinoa@gmail.com>
2024-04-04 23:23:36 +0300

    modify task_08

---
+++ urok_16.py
@@ -0,0 +1,26 @@
+import turtle
+
+v0 = int(input("Введите началбную скорость: "))
+h = int(input("Введите началбную высоту: "))
+turtle.forward(100)
+turtle.backward(100)
+turtle.left(90)
+turtle.forward(h)
+
+t = 0.1
+x = 0
+y = h
+while y > 0:
+    print(f"t={round(t, 1)}, x={round(x, 1)}, y={round(y, 1)}")
+    t += 0.1
+    x = v0 * t
+    y = h - (9.8 * t**2) / 2
+    turtle.goto(x, y)
+turtle.up()
+turtle.goto(0, h)
+turtle.write(h)
+turtle.goto(x, 0)
+turtle.write(x)
+
+
+
