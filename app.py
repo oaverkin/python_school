05ba8015e4d4a431cc0f627b40c80dfbe075bf3f
Oleg Averkin <averkinoa@gmail.com>
2024-05-02 18:36:25 +0300

    Refactor project toSQLAlchemy DB

--- app.py
+++ app.py
@@ -1,39 +1,36 @@
-import sqlite3
+import os
+
 from flask import Flask, render_template, request, url_for, flash, redirect
-from werkzeug.exceptions import abort
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'database.db')
 app.config['SECRET_KEY'] = 'your secret key'
+
+db = SQLAlchemy(app)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(nullable=False)
+    content = db.Column(nullable=False)
+    created = db.Column(db.DateTime(timezone=True),
+                        server_default=func.now())
 
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
+    posts = Posts.query.all()
     return render_template('index.html', posts=posts)
 
 
 @app.route('/<int:post_id>')
 def post(post_id):
-    post = get_post(post_id)
+    post = Posts.query.get_or_404(post_id)
     return render_template('post.html', post=post)
 
 
@@ -42,15 +39,13 @@ def create():
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-
+        post = Posts(title=title,
+                     content=content)
         if not title:
             flash('Title is required!')
         else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
-                         (title, content))
-            conn.commit()
-            conn.close()
+            db.session.add(post)
+            db.session.commit()
             return redirect(url_for('index'))
 
     return render_template('create.html')
@@ -58,21 +53,19 @@ def create():
 
 @app.route('/<int:id>/edit', methods=('GET', 'POST'))
 def edit(id):
-    post = get_post(id)
+    post = Posts.query.get_or_404(id)
 
     if request.method == 'POST':
         title = request.form['title']
         content = request.form['content']
-
         if not title:
             flash('Title is required!')
         else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
+            post.title = title
+            post.content = content
+
+            db.session.add(post)
+            db.session.commit()
             return redirect(url_for('index'))
 
     return render_template('edit.html', post=post)
@@ -80,10 +73,8 @@ def edit(id):
 
 @app.route('/<int:id>/delete', methods=('POST',))
 def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
+    post = Posts.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
     return redirect(url_for('index'))
+
